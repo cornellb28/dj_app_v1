ae4fb3fb70fed000967395a660ce5f58bcdb6763
Cornell Benson <cornellb28@gmail.com>
2023-11-07 15:33:43 -0600

    Update resource-links.js

--- src/playground/resource-links.js
+++ src/playground/resource-links.js
@@ -1,1 +1,5 @@
 // https://stackoverflow.com/questions/11194287/convert-a-directory-structure-in-the-filesystem-to-json-with-node-js
+// https://stackoverflow.com/questions/21194934/how-to-create-a-directory-if-it-doesnt-exist-using-node-js
+// https://stackoverflow.com/questions/65926112/how-to-check-whether-an-object-is-empty
+// https://www.zachgollwitzer.com/posts/file-upload-strategies-s3-nodejs-express-react-uppy
+// https://codepen.io/prvnbist/pen/jJzROe?editors=1111
